d14f1051e34f096bc332bfd666678f1dce1ca3df
code-it-vro <ashutosh2001shukla@gmail.com>
2023-08-17 21:14:05 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./styles/app.scss" 
+import {BrowserRouter as Router ,Routes , Route} from "react-router-dom"
+import Home from "./components/Home";
+import Header from "./components/Header";
+import Cart from "./components/Cart";
+import {Toaster } from "react-hot-toast";
+
+import "./styles/header.scss"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route  path="/"   element={ <Home />}/>
+        <Route  path="/cart"   element={ <Cart />}/>
+      </Routes>
+      <Toaster/>
+    </Router>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FiShoppingBag } from "react-icons/fi";
+import { useSelector } from "react-redux";
+
+const Header = () => {
+  const { cartItems } = useSelector((state) => state.cart);
+  return (
+    <nav>
+      <h2>Urban Cart </h2>
+
+      <div>
+        <Link to={"/"} className="homepage">Home</Link>
+        <Link to={"/cart"}>
+          <FiShoppingBag />
+          <p>{cartItems.length}</p>
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
